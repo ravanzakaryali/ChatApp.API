385ce9a0168b939fcee60f96f74792f996970562
revanzli <revanzekeryeli2002@gmail.com>
2022-05-09 06:30:35 +0400

    feat: add ChatHub/SendMessageAsync method

--- ChatApp.API/ChatApp.API/Hubs/ChatHub.cs
+++ ChatApp.API/ChatApp.API/Hubs/ChatHub.cs
@@ -1,6 +1,13 @@
-﻿namespace ChatApp.API.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatApp.API.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage",message);
+        }
     }
 }
