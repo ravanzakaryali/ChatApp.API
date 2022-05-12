c5bd7c82ff07c13b906dad365178a167c47ced38
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 15:06:56 +0400

    feat: added DataAccess/DbContext

---
+++ ChatApp.API/ChatApp.Data/DataAccess/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Data.DataAccess
+{
+
+    public class DbContext : IdentityDbContext<User>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Message> Messages { get; set; }  
+        
+
+    }
+}
