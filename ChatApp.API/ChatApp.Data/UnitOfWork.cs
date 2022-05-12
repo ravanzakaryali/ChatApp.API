295523542f842a69c91e1a0780e0fdbf02866704
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 23:07:06 +0400

    feat: added unit of work

---
+++ ChatApp.API/ChatApp.Data/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Core;
+using System.Threading.Tasks;
+
+namespace ChatApp.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataAccess.DbContext _context;
+        public UnitOfWork(DataAccess.DbContext context)
+        {
+            _context = context;
+        }
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
