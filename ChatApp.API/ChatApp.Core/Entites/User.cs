abee167a819382003587d09716f54321977e1575
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 14:41:54 +0400

    feat: added user model

---
+++ ChatApp.API/ChatApp.Core/Entites/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatApp.Core.Entites
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+        public string Bio { get; set; }
+    }
+}
