7f9604ab0a2cd2aadffd27947d71409bc20f5ecc
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 17:13:19 +0400

    feat: added autheticate dto

---
+++ ChatApp.API/ChatApp.Business/DTO's/Autheticate/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Business.DTO_s.Autheticate
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ChatApp.API/ChatApp.Business/DTO's/Autheticate/Register.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Business.DTO_s.Autheticate
+{
+    public class Register
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
