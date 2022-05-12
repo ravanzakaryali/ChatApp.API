6bece2a33550b3c31ec70fc36bfaf8e299f162d7
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 15:14:25 +0400

    feat: added message configuration

---
+++ ChatApp.API/ChatApp.Data/Configuration/MessageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ChatApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Data.Configuration
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.Property(m => m.SenderDate)
+                   .HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(m => m.Content)
+                   .IsRequired();
+            builder.Property(m => m.IsDeleted)
+                   .HasDefaultValue(false);
+        }
+    }
+}
