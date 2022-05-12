4f01dcaa1070eef422bcd29354124cf449651203
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 22:58:59 +0400

    feat: added repository

---
+++ ChatApp.API/ChatApp.Core/Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.Interface
+{
+    public interface IRepository<TEntity>
+    {
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> exp = null, params string[] includes);
+        Task<List<TEntity>> GetAllPaginateAsync<TOrderBy>(int page, int size, Expression<Func<TEntity, TOrderBy>> orderBy, Expression<Func<TEntity, bool>> exp = null, params string[] includes);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> exp = null, params string[] includes);
+        Task<int> GetTotalCountAsync(Expression<Func<TEntity, bool>> exp = null);
+        Task<bool> IsExistAsync(Expression<Func<TEntity, bool>> exp);
+        Task CreateAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ ChatApp.API/ChatApp.Data/Implementations/Repository.cs
@@ -0,0 +1,84 @@
+﻿using ChatApp.Core.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ChatApp.Data.Implementations
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly DataAccess.DbContext _context;
+        public Repository(DataAccess.DbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> exp = null, params string[] includes)
+
+        {
+            var query = GetQuery(includes);
+            return exp is null
+                ? await query.ToListAsync()
+                : await query.Where(exp).ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllPaginateAsync<TOrderBy>(int page, int size, Expression<Func<TEntity,
+              TOrderBy>> orderBy, Expression<Func<TEntity, bool>> exp = null, params string[] includes)
+
+        {
+            var query = GetQuery(includes);
+            return exp is null
+                ? await query.OrderByDescending(orderBy).Skip((page - 1) * size).Take(size).ToListAsync()
+                : await query.Where(exp).OrderByDescending(orderBy).Skip((page - 1) * size).Take(size).ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> exp = null, params string[] includes)
+        {
+            var query = GetQuery(includes);
+            return exp is null
+                ? await query.FirstOrDefaultAsync()
+                : await query.Where(exp).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> GetTotalCountAsync(Expression<Func<TEntity, bool>> exp = null)
+        {
+            return exp is null
+                ? await _context.Set<TEntity>().CountAsync()
+                : await _context.Set<TEntity>().Where(exp).CountAsync();
+        }
+
+        public async Task<bool> IsExistAsync(Expression<Func<TEntity, bool>> exp)
+        {
+            return await _context.Set<TEntity>().AnyAsync(exp);
+        }
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+
+        private IQueryable<TEntity> GetQuery(params string[] includes)
+        {
+            var query = _context.Set<TEntity>().AsQueryable();
+            if (includes != null)
+            {
+                foreach (var item in includes)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return query;
+        }
+    }
+}
