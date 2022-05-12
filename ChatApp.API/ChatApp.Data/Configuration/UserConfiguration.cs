9e970b19d8331b363db1af45ed579a9e3336ca26
revanzli <revanzekeryeli2002@gmail.com>
2022-05-12 15:07:49 +0400

    feat: added User Configuration

---
+++ ChatApp.API/ChatApp.Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ChatApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Data.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u=>u.UserName)
+                   .HasMaxLength(100)
+                   .IsRequired();
+            builder.Property(u=>u.Name)
+                   .HasMaxLength(100)
+                   .IsRequired();
+            builder.Property(u=>u.Surname)
+                   .HasMaxLength(100)
+                   .HasDefaultValue("XXX");
+        }
+    }
+}
