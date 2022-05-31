2a62afe0bebfd280ca7f55caa3c3583bededbbba
revanzli <revanzekeryeli2002@gmail.com>
2022-05-31 07:01:26 +0400

    feat: GetLoggedInUserName claims principal extesion

--- ChatApp.API/ChatApp.Business/Extensions/ClaimsPrincipalExtensions.cs
+++ ChatApp.API/ChatApp.Business/Extensions/ClaimsPrincipalExtensions.cs
@@ -3,13 +3,20 @@ using System.Security.Claims;
 
 namespace ChatApp.Business.Extensions
 {
-    public static class ClaimsPrincipialExtensions
+    public static class ClaimsPrincipalExtensions
     {
         public static string GetUserId(this ClaimsPrincipal principal)
         {
             if (principal is null)
                 throw new ArgumentNullException(nameof(principal));
             return principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        public static string GetLoggedInUserName(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirstValue(ClaimTypes.Name);
         }
     }
 }
