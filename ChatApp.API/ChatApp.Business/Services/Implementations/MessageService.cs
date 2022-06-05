36ee01851faf90a6513c59d2f0678bdccbe48911
revanzli <revanzekeryeli2002@gmail.com>
2022-06-05 14:11:27 +0400

    fix: getMessages request

--- ChatApp.API/ChatApp.Business/Services/Implementations/MessageService.cs
+++ ChatApp.API/ChatApp.Business/Services/Implementations/MessageService.cs
@@ -40,7 +40,7 @@ namespace ChatApp.Business.Services.Implementations
                 throw new NotFoundException("User is not defined");
             }
             var loginUserId = _httpContext.HttpContext.User.GetUserId();
-            var messagesDb = await _unitOfWork.MessageRepository.GetAllPaginateAsync(1,10,m=>m.SenderDate,m=>m.SendUserId == loginUserId && m.UserId == user.Id);
+            var messagesDb = await _unitOfWork.MessageRepository.GetAllPaginateAsync(1,10,m=>m.SenderDate,m=>(m.SendUserId == loginUserId && m.UserId == user.Id) || (m.SendUserId == user.Id && m.UserId == loginUserId));
             return _mapper.Map<List<GetMessage>>(messagesDb);
         }
     }
